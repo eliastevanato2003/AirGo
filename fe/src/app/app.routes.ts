bd7337880d92c4171e4a29e3e9c4ea94b131a12f
Dario Caberlotto <dariocaberlotto@gmail.com>
2025-09-01 09:27:42 +0200

    fe dir added

---
+++ fe/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserSignupComponent } from './user-signup/user-signup.component';
+import { FlightsComponent } from './flights/flights.component';
+
+export const routes: Routes = [
+    { path: '', component: FlightsComponent},
+    { path: 'login', component: UserLoginComponent },
+    { path: 'signup', component: UserSignupComponent }
+];
