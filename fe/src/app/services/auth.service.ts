ef53ab53bc384047c69b61c7c52500de60f20d97
Dario Caberlotto <dariocaberlotto@gmail.com>
2025-09-03 12:39:16 +0200

    minor changes

--- fe/src/app/services/auth.service.ts
+++ fe/src/app/services/auth.service.ts
@@ -1,12 +1,19 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private loggedIn$ = new BehaviorSubject<boolean>(false);
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (this.isBrowser()) {
+      const token = localStorage.getItem('authToken');
+      this.loggedIn$.next(!!token);
+    }
+  }
 
   private isBrowser(): boolean {
     return isPlatformBrowser(this.platformId);
@@ -14,7 +21,10 @@ export class AuthService {
 
   // Salva il token in localStorage
   login(token: string) {
-    if(this.isBrowser()) localStorage.setItem('authToken', token);
+    if(this.isBrowser()) {
+      localStorage.setItem('authToken', token);
+      this.loggedIn$.next(true);
+    }
   }
 
   // Ottieni il token da localStorage
@@ -24,7 +34,10 @@ export class AuthService {
 
   // Rimuovi il token da localStorage
   logout() {
-    if(this.isBrowser()) localStorage.removeItem('authToken');
+    if(this.isBrowser()) {
+      localStorage.removeItem('authToken');
+      this.loggedIn$.next(false);
+    }
   }
 
   // Esempio con sessionStorage
@@ -34,5 +47,9 @@ export class AuthService {
 
   getTemporaryData(): string | null {
     return this.isBrowser() ? sessionStorage.getItem('tempData'): null;
+  }
+
+  isLoggedIn() {
+    return this.loggedIn$.asObservable();
   }
 }
