f944bd1f1c3d626afbd48623268fb795189caef5
Uomoninja189 <897778@stud.unive.com>
2025-09-08 21:37:03 +0200

    buyticket and airplanes components

--- fe/src/app/app.routes.ts
+++ fe/src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { UserSignupComponent } from './user-signup/user-signup.component';
 import { FlightsComponent } from './flights/flights.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { FlightsListComponent } from './flightslist/flightslist.component';
+import { BuyticketComponent } from './buyticket/buyticket.component';
+import { AirplanesComponent } from './airplanes/airplanes.component';
 
 export const routes: Routes = [
     { path: '', component: FlightsComponent},
     { path: 'login', component: UserLoginComponent },
     { path: 'signup', component: UserSignupComponent },
     { path: 'profile', component: UserProfileComponent },
-    { path: 'flightslist', component: FlightsListComponent }
+    { path: 'flightslist', component: FlightsListComponent },
+    { path: 'buyticket', component: BuyticketComponent},
+    { path: 'airplanes', component: AirplanesComponent }
 ];
