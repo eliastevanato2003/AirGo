c29fed6b1408d1af37950e454ccbd7fe7399be0d
Dario Caberlotto <dariocaberlotto@gmail.com>
2025-09-10 12:52:57 +0200

    minor changes (todo delete)

--- fe/src/app/components/admin/airlines/airlines.component.ts
+++ fe/src/app/components/admin/airlines/airlines.component.ts
@@ -14,21 +14,17 @@ export class AirlinesComponent implements OnInit {
   airlines: Airline[] | null = [];
   newAirline: NewAirline = { name: '', code: '', email: '', password: 'password' };
 
-  public airlineForm;
+  public airlineForm: FormGroup | any;
 
   constructor(private airlineService: AirlineService) {
-    this.airlineForm = new FormGroup({
-      name: new FormControl(''),
-      code: new FormControl(''),
-      email: new FormControl('')
-    });
+    this.resetForm();
   }
 
   ngOnInit(): void {
     this.loadAirlines();
   }
 
-  async loadAirlines() {
+  loadAirlines() {
     this.airlineService.getAirlines().subscribe({
       next: (response) => {
         this.airlines = response as Airline[];
@@ -41,14 +37,25 @@ export class AirlinesComponent implements OnInit {
     this.newAirline.name = formData.name!;
     this.newAirline.code = formData.code!;
     this.newAirline.email = formData.email!;
-    this.airlineService.addAirline(this.newAirline);
+    this.airlineService.addAirline(this.newAirline).subscribe(() => {
+      this.loadAirlines();
+      this.resetForm();
+    });
     this.newAirline = { name: '', code: '', email: '', password: 'password' };
   }
 
   // TODO: finire delete
   deleteAirline(id: number): void {
     if (confirm('Sei sicuro di voler eliminare questa compagnia aerea?')) {
-      //this.airlineService.deleteAirline(id).subscribe(() => this.loadAirlines());
+      this.airlineService.deleteAirline(id).subscribe(() => this.loadAirlines());
     }
+  }
+
+  resetForm() {
+    this.airlineForm = new FormGroup({
+      name: new FormControl(''),
+      code: new FormControl(''),
+      email: new FormControl('')
+    });
   }
 }
